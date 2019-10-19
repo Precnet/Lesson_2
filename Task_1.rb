3c7b955d7388bdce046659464bc20350d9bdef8f
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-19 23:06:09 +0300

    constant with months days function to create hash

--- Task_1.rb
+++ Task_1.rb
@@ -2,5 +2,8 @@
 # Output those with 30 days
 
 MONTHS_NAMES = %w(january february march april may june july august september october novemner december)
+MONTHS_LENGTH_IN_DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-
+def create_months
+  MONTHS_NAMES.zip(MONTHS_LENGTH_IN_DAYS).to_h
+end
