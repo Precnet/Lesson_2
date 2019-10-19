cc4b014164a71f130561f2853f545071582777c3
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-19 23:40:44 +0300

    final changes

--- Task_3.rb
+++ Task_3.rb
@@ -1,2 +1,14 @@
 # Fill array with Fibonacci`s numbers upto 100
 
+def generate_fibonacci_upto(border = 100)
+  result = [0, 1]
+  result.push(generate_fibonacci_next(result[-2], result[-1])) while  result[-1] < border
+  result[0..-2]
+end
+
+def generate_fibonacci_next(first, second)
+  first + second
+end
+
+p generate_fibonacci_upto
+p generate_fibonacci_upto 300
