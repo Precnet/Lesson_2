6541338ad31b93f4ca270ade8a19c69ce769186d
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-20 00:12:44 +0300

    renamed constants

--- Task_5.rb
+++ Task_5.rb
@@ -2,8 +2,8 @@
 # outputs day number from the beginning of the year
 # check year for leap
 
-MONTHS_LENGTH_IN_DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-MONTHS_LENGTH_IN_DAYS_LEAP = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+MONTHS_LENGTH_IN_DAYS_STANDARD_YEAR = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+MONTHS_LENGTH_IN_DAYS_LEAP_YEAR = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def create_year(day, month, year)
   {day: day, month: month, year: year}
