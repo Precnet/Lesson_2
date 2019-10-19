68d7f63b0ea9b5adf2bd04f4f22583beeb8a01bd
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-20 01:17:49 +0300

    added main loog

--- Task_6.rb
+++ Task_6.rb
@@ -1,8 +1,21 @@
 STOP = 'stop'
 
+def fill_the_basket(basket)
+  while true
+    user_input = get_input
+    break if is_stop? user_input
+    purchase = extract_data_from_string(user_input)
+    add_purchase_to_basket(basket, purchase)
+  end
+end
+
 def get_input
   puts 'Enter product name, price per item, number of items (Example: bread 2.4 4):'
   gets.chomp
+end
+
+def is_stop?(user_input)
+  user_input == STOP
 end
 
 def extract_data_from_string(string)
@@ -10,8 +23,10 @@ def extract_data_from_string(string)
   [data[0], [data[1].to_f, data[2].to_i]]
 end
 
-def add_purchase_to_baslet(basket, purchase)
+def add_purchase_to_basket(basket, purchase)
   basket[purchase[0]] = purchase[1]
 end
 
-busket = {}
+basket = {}
+fill_the_basket basket
+p basket
