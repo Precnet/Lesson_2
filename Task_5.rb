4d340e51dbd9916e499ae1f5bad8add9c9c899e9
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-20 00:06:36 +0300

    added function for year creation added function for checking of leap year

--- Task_5.rb
+++ Task_5.rb
@@ -2,4 +2,10 @@
 # outputs day number from the beginning of the year
 # check year for leap
 
+def create_year(day, month, year)
+  {day: day, month: month, year: year}
+end
 
+def is_leap?(year)
+  year[year] % 4 == 0 && year[year] % 100 != 0 && year[year] % 400 != 0
+end
