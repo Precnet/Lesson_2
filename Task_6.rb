162739bf7c0de60bc15b1430e61229ac431311bc
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-20 01:07:52 +0300

    added function to string format

--- Task_6.rb
+++ Task_6.rb
@@ -1,6 +1,11 @@
 STOP = 'stop'
 
 def get_input
-  puts 'Enter product name, price per item, number of items (Exmple: bread 2.4 4):'
+  puts 'Enter product name, price per item, number of items (Example: bread 2.4 4):'
   gets.chomp
+end
+
+def extract_data_from_string(string)
+  data = string.split(' ')
+  [data[0], [data[1].to_f, data[2].to_i]]
 end
