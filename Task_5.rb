7cdd317c1c06b45734d6c28ed21b48b5cb8e37cf
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-20 00:12:21 +0300

    added constants with months duratations for standard anf leap years

--- Task_5.rb
+++ Task_5.rb
@@ -2,10 +2,13 @@
 # outputs day number from the beginning of the year
 # check year for leap
 
+MONTHS_LENGTH_IN_DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+MONTHS_LENGTH_IN_DAYS_LEAP = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
 def create_year(day, month, year)
   {day: day, month: month, year: year}
 end
 
-def is_leap?(year)
-  year[year] % 4 == 0 && year[year] % 100 != 0 && year[year] % 400 != 0
+def is_leap?(current_year)
+  current_year[:year] % 4 == 0 && current_year[:year] % 100 != 0 && current_year[:year] % 400 != 0
 end
