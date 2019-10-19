42b9d35afaacd07e5b319da8a8e3ee26722588fc
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-20 01:46:37 +0300

    replaced with new report function

--- Task_6.rb
+++ Task_6.rb
@@ -27,17 +27,26 @@ def add_purchase_to_basket(basket, purchase)
   basket[purchase[0]] = purchase[1]
 end
 
-def print_basket(basket)
+def print_basket_raw(basket)
   puts "Your purchases (name=>[price, amount]):\n#{basket}"
 end
 
-def print_total_price_for_each_positition(basket)
+def print_basket_report(basket)
+  report_data = prepare_report_data basket
   puts 'Total price per position:'
-  basket.each_key { |key| puts "#{key}: \t#{basket[key][0] * basket[key][1]}" }
+  report_data.each_key {|key| puts "#{key}: \t#{report_data[key][0] * report_data[key][1]}" }
+  puts 'Total price:'
+  puts report_data.values.sum
+end
+
+def prepare_report_data(basket)
+  report_data = {}
+  basket.each_key { |key| report_data[key] = basket[key][0] * basket[key][1]}
+  report_data
 end
 
 basket = {}
 fill_basket basket
 
-print_basket basket
-print_total_price_for_each_positition basket
+print_basket_raw basket
+print_basket_report basket
