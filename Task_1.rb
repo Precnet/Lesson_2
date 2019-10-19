9e389f6345f730ad554d4dc53b2007938509d3c6
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-19 23:13:43 +0300

    final lunch

--- Task_1.rb
+++ Task_1.rb
@@ -8,6 +8,8 @@ def create_months
   MONTHS_NAMES.zip(MONTHS_LENGTH_IN_DAYS).to_h
 end
 
-def print_months_by_days(months, days_in_month = 30)
-  months.each {|month| month if months[month] == days_in_month}
+def get_months_by_days(months, days_in_month = 30)
+  months.select {|month| months[month] == days_in_month}
 end
+
+print get_months_by_days(create_months)
