0c939c776727493f48f800599d5427d72921b6b0
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-19 23:09:24 +0300

    function to output months name according to condition

--- Task_1.rb
+++ Task_1.rb
@@ -7,3 +7,7 @@ MONTHS_LENGTH_IN_DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 def create_months
   MONTHS_NAMES.zip(MONTHS_LENGTH_IN_DAYS).to_h
 end
+
+def print_months_by_days(months, days_in_month = 30)
+  months.each {|month| month if months[month] == days_in_month}
+end
