f753ede5d58bee2c27a283a0333908477d668b80
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-19 23:21:34 +0300

    added some checking

--- Task_2.rb
+++ Task_2.rb
@@ -4,4 +4,5 @@ def generate_array_with_step(start = 10, stop = 100, step = 5)
   (start..stop).step(step).to_a
 end
 
-print generate_array_with_step
+p generate_array_with_step
+p generate_array_with_step(100, 200, 10)
