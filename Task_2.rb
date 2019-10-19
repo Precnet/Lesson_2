59997f49138383ce231e990c8c73a60c5e397fb3
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-19 23:19:47 +0300

    added function for array generation

--- Task_2.rb
+++ Task_2.rb
@@ -1,1 +1,7 @@
 # Fill array with numbers from 10 to 100 with step=5
+
+def generate_array_with_step(start = 10, stop = 100, step = 5)
+  (start..stop).step(step).to_a
+end
+
+print generate_array_with_step
