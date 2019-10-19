0cbb37d6ecc613fe3eca1a07b46a36b8db2e00fe
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-19 22:54:01 +0300

    added constant with months names

--- Task_1.rb
+++ Task_1.rb
@@ -1,4 +1,6 @@
-# Create Hash with monthes as keys ans days as values
+# Create Hash with months as keys ans days as values
 # Output those with 30 days
+
+MONTHS_NAMES = %w(january february march april may june july august september october novemner december)
 
 
