68377998a54831251ace4f71400ef5d50a04a0e0
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-19 23:55:35 +0300

    function for hash creation

--- Task_4.rb
+++ Task_4.rb
@@ -6,4 +6,10 @@ def create_alphabet
   ('a'..'z').to_a
 end
 
-print create_alphabet
+def get_vowels(alphabet = create_alphabet)
+  result = {}
+  VOWELS.each { |vowel| result[vowel] = alphabet.find_index(vowel) + 1 }
+  result
+end
+
+print get_vowels
